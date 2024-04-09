c2c442b068e1dfb3e9b89c62fba9ea6ba3661617
Gleb <glebzubkov1239879@gmail.com>
2024-04-09 17:52:25 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,7 @@
+﻿const menu = document.querySelector('.menu');
+const hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger_active');
+    menu.classList.toggle('menu_active');
+})
