57e4f3029420e5fac01f48c495a80b7960e8cf76
Gleb <glebzubkov1239879@gmail.com>
2024-04-11 16:05:21 +0300

    change script.js

--- js/script.js
+++ js/script.js
@@ -4,4 +4,4 @@ const hamburger = document.querySelector('.hamburger');
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('hamburger_active');
     menu.classList.toggle('menu_active');
-})
+});
